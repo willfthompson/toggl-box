3ce504d91149320cc7f380ce456d6aa191c2e4ec
Will Thompson <67681857+willfthompson@users.noreply.github.com>
2021-03-06 12:28:59 +0000

    Update toggle-box.py

--- toggle-box.py
+++ toggle-box.py
@@ -22,27 +22,27 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
 #Toggl Authorisation Goes Here
-AUTH = "9ff7813fb8fc6b53cb2a7101875e85e8"
+AUTH = "Your Auth code"
 
 # Up to 12 Clients can be used. But all buttons must be filled
 CLIENTS = [
-["Ecospend", 165981673, (255,0,0)],
-["Ohpen", 156584840, (255,127,0)],
-["Amberscript", 167653344, (255,255,0)],
-["Something Ventured", 167653396, (0,255,0)],
-["Whering", 167653351, (0,149,77)],
-["BabyDuet", 161637546, (45,200,77)],
-["New Options", 167653385,(7,175,212)],
-["Gravity", 164655916,(0,0,255)],
-["Dotiv", 167653496, (0,0,255)],
-["OneE", 167653362, (224,60,49)],
-["Mum", 157881720, (174,118,163)],
-["Nothing Ventured", 167653389,(209,187,215)],
+["name", 0000000000, (255,0,0)],
+["name", 000000000, (255,127,0)],
+["name", 000000000, (255,255,0)],
+["name", 00000000, (0,255,0)],
+["name", 00000000, (0,149,77)],
+["name", 00000000, (45,200,77)],
+["name", 00000000,(7,175,212)],
+["name", 00000000,(0,0,255)],
+["name", 00000000, (0,0,255)],
+["name", 00000000, (224,60,49)],
+["name", 00000000, (176,8,143)],
+["name", 00000000,(255,188,0)],
 # The last 4 buttons are assigned to tags which do not carry an ID
 ["Admin", 0, (255,255,255)],
 ["Planning", 0, (255,255,255)],
 ["Meetings & Calls", 0, (255,255,255)],
-["Media", 0,(255,255,255)],
+["Doing", 0,(255,255,255)],
 # Final element is a Default tag can be customised as desired.
 [""]
 ]
@@ -52,6 +52,49 @@ ON = [False] * 12
 id = 0
 tag = 16
 
+# this will be called on incorrect inputs to flash the requested button at the assigned color
+def flash(e):
+    for i in range(3):
+        trellis.pixels[e] = CLIENTS[e][2]
+        time.sleep(0.1)
+        trellis.pixels[e] = OFF
+        time.sleep(0.1)
+
+#This will be called when the tag should be reset to default
+def defaulttag(e):
+    tag = 16
+    trellis.pixels[e] = OFF
+
+#Will be called when a timer stop needs to be sent to Toggl
+def stoptimer(e):
+    trellis.pixels[e] = OFF
+    ON[e] = False
+    headers = {
+    'Content-Type': 'application/json',
+    }
+    requests.put((f'https://api.track.toggl.com/api/v8/time_entries/{id}/stop'), headers=headers, auth=(f'{AUTH}', 'api_token'))
+
+def starttimer(e,t):
+    trellis.pixels[e] = CLIENTS[e][2]
+    
+    # This starts a timer
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    data = '''{"time_entry":{
+    "tags":["%s"],
+    "pid":"%s",
+    "created_with":"toggl-box"}}'''%(CLIENTS[t][0] ,CLIENTS[e][1])
+                         
+    response = requests.post('https://api.track.toggl.com/api/v8/time_entries/start', headers=headers, data=data, auth=(f'{AUTH}', 'api_token'))
+    if response.ok:
+        ON[e] = True
+        id = response.json()['data']['id']
+    else:
+        print(response.status_code)
+        flash(e)
+    
 # this will be called when button events are received
 def blink(event):
     global id
@@ -60,22 +103,12 @@ def blink(event):
     # Button is for a tag
     if event.edge == NeoTrellis.EDGE_RISING and event.number > 11:
         if True in ON:
-            if tag == event.number:
-                #No tags currently so reset to default tag
-                tag = 16
-                trellis.pixels[event.number] = OFF
-            else:
-                for i in range(3):
-                    trellis.pixels[event.number] = CLIENTS[event.number][2]
-                    time.sleep(0.2)
-                    trellis.pixels[event.number] = OFF
-                    time.sleep(0.2)
+            flash(event.number)
         else:
             # Check if current tag is being turned off
             if tag == event.number:
                 #No tags currently so reset to default tag
-                tag = 16
-                trellis.pixels[event.number] = OFF
+                defaulttag(event.number)
             # Else it's a new tag
             else:
                 # Turn off the tag button light UNLESS it is default
@@ -90,42 +123,11 @@ def blink(event):
         if event.edge == NeoTrellis.EDGE_RISING and ON[event.number] == False:
             # Check for already running timers
             if True in ON:
-                trellis.pixels[ON.index(True)] = OFF
-                ON[ON.index(True)] = False
-                headers = {
-                'Content-Type': 'application/json',
-                }
-                requests.put((f'https://api.track.toggl.com/api/v8/time_entries/{id}/stop'), headers=headers, auth=(f'{AUTH}', 'api_token'))
-                    
-            trellis.pixels[event.number] = CLIENTS[event.number][2]
-            ON[event.number] = True
-
-            # This starts a timer
-            headers = {
-                'Content-Type': 'application/json',
-            }
-
-            data = '''{"time_entry":{
-            "tags":["%s"],
-            "pid":"%s",
-            "created_with":"toggl-box"}}'''%(CLIENTS[tag][0] ,CLIENTS[event.number][1])
-            
-                   
-            response = requests.post('https://api.track.toggl.com/api/v8/time_entries/start', headers=headers, data=data, auth=(f'{AUTH}', 'api_token'))
-            id = response.json()['data']['id']
-
-
+                stoptimer(ON.index(True))
+            starttimer(event.number, tag)
         elif event.edge == NeoTrellis.EDGE_RISING and ON[event.number] == True:
-            trellis.pixels[event.number] = OFF
-            trellis.pixels[tag] = OFF
-            tag = 16
-            ON[event.number] = False
-            headers = {
-                'Content-Type': 'application/json',
-            }
-            requests.put((f'https://api.track.toggl.com/api/v8/time_entries/{id}/stop'), headers=headers, auth=(f'{AUTH}', 'api_token'))
-
-          
+            stoptimer(event.number)
+            defaulttag(tag) 
 
 for i in range(16):
     # activate rising edge events on all keys
@@ -134,7 +136,6 @@ for i in range(16):
     trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
     # set all keys to trigger the blink callback
     trellis.callbacks[i] = blink
-
     # cycle the LEDs on startup
     trellis.pixels[i] = CLIENTS[i][2]
     time.sleep(0.05)
@@ -150,3 +151,6 @@ while True:
     trellis.sync()
     # the trellis can only be read every 17 millisecons or so
     time.sleep(0.02)
+
+
+
