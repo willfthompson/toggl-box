51211c86e60cdffc93ead08014ddb500159b3318
Will Thompson <67681857+willfthompson@users.noreply.github.com>
2021-03-06 12:29:53 +0000

    Update toggl-pid-finder.py

--- toggl-pid-finder.py
+++ toggl-pid-finder.py
@@ -1,4 +1,4 @@
 import requests
-response = requests.get((f'https://api.track.toggl.com/api/v8/workspaces/2148194/projects'), auth=('9ff7813fb8fc6b53cb2a7101875e85e8', 'api_token'))
+response = requests.get((f'https://api.track.toggl.com/api/v8/workspaces/2148194/projects'), auth=('Your auth code', 'api_token'))
 
 print(response.json())
